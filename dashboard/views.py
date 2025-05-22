d073e93f3f4706f9c3ea5396b969cb66f5113a1d
namem <namem_br@hotmail.com>
2025-05-22 14:06:51 -0400

    comecando ranking

--- dashboard/views.py
+++ dashboard/views.py
@@ -3,6 +3,8 @@ from django.shortcuts import render
 from chamados.models import Chamado
 from django.utils import timezone
 from django.utils.timezone import localdate
+from django.db.models import Count
+from django.contrib.auth.models import User
 
 @login_required
 def dashboard(request):
@@ -12,13 +14,22 @@ def dashboard(request):
     abertos_hoje = Chamado.objects.filter(data_abertura__date=localdate(), status='ABERTO').count()
     total_em_atendimento = Chamado.objects.filter(status='EM_ATENDIMENTO').count()
 
+    ranking = (
+        User.objects
+        .annotate(qtd_chamados=Count('chamado'))
+        .order_by('-qtd_chamados')[:10]
+    )
 
+    labels = [user.username for user in ranking]
+    data = [user.qtd_chamados for user in ranking]
 
     contexto = {
         'total_abertos': total_abertos,
         'total_encerrados': total_encerrados,
         'abertos_hoje': abertos_hoje,
         'total_em_atendimento': total_em_atendimento,
+        'labels': labels,
+        'data': data,
     }
 
     return render(request, 'dashboard/index.html', contexto)
