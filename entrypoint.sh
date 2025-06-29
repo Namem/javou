e5d30fdea8d155eb384ef049e52bf55c602040e6
namem <namem_br@hotmail.com>
2025-06-28 22:16:29 -0400

    Simplifica entrypoint para deploy no Render

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,17 +1,14 @@
 #!/bin/sh
 
+# O script irá parar se qualquer comando falhar
 set -e
 
-echo "Aguardando o banco de dados..."
-while ! nc -z $POSTGRES_HOST $POSTGRES_PORT; do
-  sleep 0.1
-done
-echo "Banco de dados disponível."
-
+# Executa os comandos de setup do Django
 echo "Aplicando migrações do banco de dados..."
 python manage.py migrate --noinput
 
 echo "Coletando arquivos estáticos..."
 python manage.py collectstatic --noinput
 
+# Inicia o processo principal (o comando que foi passado para o container, ex: gunicorn)
 exec "$@"
